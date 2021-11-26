7c7b83202d8c384e219e46eb593cb1380b385c3f
alejandrocheca <alexcheca95@gmail.com>
2021-11-26 13:09:09 +0100

    Update controller image

--- src/main/java/com/pizzeria/pizzeria/controller/imageController/ImageController.java
+++ src/main/java/com/pizzeria/pizzeria/controller/imageController/ImageController.java
@@ -1,44 +1,44 @@
-/*package com.pizzeria.pizzeria.controller.imageController;
+package com.pizzeria.pizzeria.controller.imageController;
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import com.pizzeria.pizzeria.application.imageApplication.CreateOrUpdateImageDTO;
+import com.pizzeria.pizzeria.application.imageApplication.ImageApplication;
+import com.pizzeria.pizzeria.application.imageApplication.ImageDTO;
+
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import reactor.core.publisher.Mono;
 
+@RestController
 public class ImageController {
-    private final ImageService imageService;
-
-    @PostMapping("/image")
-    Mono<CreateImageResponse> create(@RequestBody CreateImageRequest request) {
-        return imageService.shortenImage(request.getImage())
-                          .map(CreateImageResponse::new);
+    private ImageApplication imageApplication;
+    @Autowired
+    public ImageController(final ImageApplication imageApplication){
+        this.imageApplication = imageApplication;
+    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/{id}")
+    public Mono<ImageDto> getImage(@PathVariable UUID id) {
+        return imageApplication.findById(id);
+    }
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<ImageDto> add(@RequestParam("image") MultipartFile multipartFile) throws Exception {
+        return imageApplication.add(multipartFile);
     }
 
-    @GetMapping("/{byte[]}")
-    Mono<ResponseEntity<Object>> getImagek(@PathVariable String key) {
-        return imageService.getOriginalImage(key)
-                          .map(image -> ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT)
-                                                     .header("Location", link.getOriginalLink())
-                                                     .build())
-                          .defaultIfEmpty(ResponseEntity.notFound().build());
-    }
-
-    @Value
-    public static class CreateImageRequest {
-
-        private String image;
-    }
-
-    @Value
-    public static class CreateImageResponse {
-
-        private String shortenedImage;
-    }  
 }
-*/

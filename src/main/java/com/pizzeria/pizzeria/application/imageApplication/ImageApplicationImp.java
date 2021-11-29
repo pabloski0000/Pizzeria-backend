ade067f746b7e3d02ac1d30d59d686a24e081018
alejandrocheca <alexcheca95@gmail.com>
2021-11-29 10:24:09 +0100

    Update ImageApplicationImp in application

--- src/main/java/com/pizzeria/pizzeria/application/imageApplication/ImageApplicationImp.java
+++ src/main/java/com/pizzeria/pizzeria/application/imageApplication/ImageApplicationImp.java
@@ -1,19 +1,16 @@
 package com.pizzeria.pizzeria.application.imageApplication;
 
-import java.io.IOException;
 import java.util.UUID;
-
 import com.pizzeria.pizzeria.domain.imageDomain.Image;
 import com.pizzeria.pizzeria.domain.imageDomain.ImageRepository;
-
+import com.pizzeria.pizzeria.domain.ingredientDomain.Ingredient;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import javassist.NotFoundException;
 import reactor.core.publisher.Mono;
-
-import com.pizzeria.pizzeria.core.ApplicationBase;
-import com.pizzeria.pizzeria.core.functionalInterfaces.FindById;
 
 @Service
 public class ImageApplicationImp extends ApplicationBase<Image, UUID> implements ImageApplication{
@@ -21,14 +18,37 @@ public class ImageApplicationImp extends ApplicationBase<Image, UUID> implements
     private final ImageRepository imageRepository;
     private final ModelMapper modelMapper;
     
-
-    public ImageDTO save(CreateOrUpdateImageDTO dto) throws IOException{
-        throw new RuntimeException();
+    public ImageApplicationImpl(final ImageRepository imageRepository, final ModelMapper modelMapper){
+        this.imageRepository = imageRepository;
+        this.modelMapper = modelMapper;
     }
-
-    public BytesDTO get(UUID id){
-        throw new RuntimeException();
+    @Override
+    public Mono<ImageDTO> add(MultipartFile multipartFile) throws Exception {
+        CreateOrUpdateImageDTO createOrUpdateImageDto = new CreateOrUpdateImageDto();
+        createOrUpdateImageDto.setContent(multipartFile.getBytes());
+        Image image = modelMapper.map(createOrUpdateImageDto, Image.class);
+        image.setId(UUID.randomUUID());
+        image.setThisNew(true);
+        return imageRepository.add(image)
+            .flatMap(added -> {
+                if(!added){
+                    //Look error code up
+                    return Mono.error(new Exception("The image wasn't saved correctly: " + createOrUpdateImageDto.getContent().toString()));
+                }
+                return Mono.just(modelMapper.map(image, ImageDto.class));
+            });
     }
-
+    @Override
+    public Mono<ImageDto> findById(UUID id) {
+        return imageRepository
+            .findById(id)
+            .switchIfEmpty(Mono.error(new NotFoundException("There isn't any image with id: " + id)))
+            .map(contentImageInBytes -> {
+                Image image = new Image();
+                image.setId(id);
+                image.setContent(contentImageInBytes);
+                return modelMapper.map(image, ImageDto.class);
+            });
+    }
     
 }
